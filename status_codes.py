1a0e4d716805d07b001c6b74e92089f8033f4cae
dukakisxyz <kdukakis@icloud.com>
2016-07-18 21:32:55 +0300

    python3 compatible & various fixes, see description
    return the result in the form of a json dictionary + bug fixes

--- status_codes.py
+++ status_codes.py
@@ -1,165 +1,165 @@
 def check_status(status):
 	#2xx Success
 	if status == 200:
-		return ("200 OK")
+		return ("OK")
 	elif status == 201:
-		return ("201 Created")
+		return ("Created")
 	elif status == 202:
-		return ("202 Accepted")
+		return ("Accepted")
 	elif status == 203:
-		return ("203 Non-Authoritative Information")
+		return ("Non-Authoritative Information")
 	elif status == 204:
-		return ("204 No Content")
+		return ("No Content")
 	elif status == 205:
-		return ("205 Reset Content")
+		return ("Reset Content")
 	elif status == 206:
-		return ("206 Partial Content")
+		return ("Partial Content")
 	elif status == 207:
-		return ("207 Multi-Status")
+		return ("Multi-Status")
 	elif status == 208:
-		return ("208 Already Reported")
+		return ("Already Reported")
 	elif status == 226:
-		return ("226 IM Used")
+		return ("IM Used")
 
 	#3xx Redirection
 	elif status == 300:
-		return ("300 Multiple Choices")
+		return ("Multiple Choices")
 	elif status == 301:
-		return ("301 Moved Permanently")
+		return ("Moved Permanently")
 	elif status == 302:
-		return ("302 Found")
+		return ("Found")
 	elif status == 303:
-		return ("303 See Other")
+		return ("See Other")
 	elif status == 304:
-		return ("304 Not Modified")
+		return ("Not Modified")
 	elif status == 305:
-		return ("305 Use Proxy")
+		return ("Use Proxy")
 	elif status == 306:
-		return ("306 Switch Proxy")
+		return ("Switch Proxy")
 	elif status == 307:
-		return ("307 Temporary Redirect")
+		return ("Temporary Redirect")
 	elif status == 308:
-		return ("308 Permanent Redirect")
+		return ("Permanent Redirect")
 
 	#4xx Client Error
 	elif status == 400:
-		return ("400 Bad Request")
+		return ("Bad Request")
 	elif status == 401:
-		return ("401 Unauthorized")
+		return ("Unauthorized")
 	elif status == 402:
-		return ("402 Payment Required")
+		return ("Payment Required")
 	elif status == 403:
-		return ("403 Forbidden")
+		return ("Forbidden")
 	elif status == 404:
-		return ("404 Not Found")
+		return ("Not Found")
 	elif status == 405:
-		return ("405 Method Not Allowed")
+		return ("Method Not Allowed")
 	elif status == 406:
-		return ("406 Not Acceptable")
+		return ("Not Acceptable")
 	elif status == 407:
-		return ("407 Proxy Authentication Required")
+		return ("Proxy Authentication Required")
 	elif status == 408:
-		return ("408 Request Timeout")
+		return ("Request Timeout")
 	elif status == 409:
-		return ("409 Conflict")
+		return ("Conflict")
 	elif status == 410:
-		return ("410 Gone")
+		return ("Gone")
 	elif status == 411:
-		return ("411 Length Required")
+		return ("Length Required")
 	elif status == 412:
-		return ("412 Precondition Failed")
+		return ("Precondition Failed")
 	elif status == 413:
-		return ("413 Payload Too Large")
+		return ("Payload Too Large")
 	elif status == 414:
-		return ("414 URI Too Long")
+		return ("URI Too Long")
 	elif status == 415:
-		return ("415 Unsupported Media Type")
+		return ("Unsupported Media Type")
 	elif status == 416:
-		return ("416 Range Not Satisfiable")
+		return ("Range Not Satisfiable")
 	elif status == 417:
-		return ("417 Expectation Failed")
+		return ("Expectation Failed")
 	elif status == 418:
-		return ("418 I'm a teapot")
+		return ("I'm a teapot")
 	elif status == 421:
-		return ("421 Misdirected Request")
+		return ("Misdirected Request")
 	elif status == 422:
-		return ("422 Unprocessable Entity")
+		return ("Unprocessable Entity")
 	elif status == 423:
-		return ("423 Locked")
+		return ("Locked")
 	elif status == 424:
-		return ("424 Failed Dependency")
+		return ("Failed Dependency")
 	elif status == 426:
-		return ("426 Upgrade Required")
+		return ("Upgrade Required")
 	elif status == 428:
-		return ("428 Precondition Required")
+		return ("Precondition Required")
 	elif status == 429:
-		return ("429 Too Many Requests")
+		return ("Too Many Requests")
 	elif status == 431:
-		return ("431 Request Header Fields Too Large")
+		return ("Request Header Fields Too Large")
 	elif status == 451:
-		return ("451 Unavailable For Legal Reasons")
+		return ("Unavailable For Legal Reasons")
 
 	elif status == 440:
-		return ("440 Login Timeout")
+		return ("Login Timeout")
 	elif status == 449:
-		return ("449 Retry With")
+		return ("Retry With")
 
 	#5xx Server Error
 	elif status == 500:
-		return ("500 Internal Server Error")
+		return ("Internal Server Error")
 	elif status == 501:
-		return ("501 Not Implemented")
+		return ("Not Implemented")
 	elif status == 502:
-		return ("502 Bad Gateway")
+		return ("Bad Gateway")
 	elif status == 503:
-		return ("503 Service Unavailable")
+		return ("Service Unavailable")
 	elif status == 504:
-		return ("504 Gateway Timeout")
+		return ("Gateway Timeout")
 	elif status == 505:
-		return ("505 HTTP Version Not Supported")
+		return ("HTTP Version Not Supported")
 	elif status == 506:
-		return ("506 Variant Also Negotiates")
+		return ("Variant Also Negotiates")
 	elif status == 507:
-		return ("507 Insufficient Storage")
+		return ("Insufficient Storage")
 	elif status == 508:
-		return ("508 Loop Detected")
+		return ("Loop Detected")
 	elif status == 510:
-		return ("510 Not Extended")
+		return ("Not Extended")
 	elif status == 511:
-		return ("511 Network Authentication Required")
+		return ("Network Authentication Required")
 
 	#Unofficial codes
 	elif status == 103:
-		return ("103 Checkpoint")
+		return ("Checkpoint")
 	elif status == 450:
-		return ("450 Blocked by Windows Parental Controls")
+		return ("Blocked by Windows Parental Controls")
 	elif status == 509:
-		return ("509 Bandwidth Limit Exceeded")
+		return ("Bandwidth Limit Exceeded")
 
 	#nginx
 	elif status == 444:
-		return ("444 No Response (nginx)")
+		return ("No Response (nginx)")
 	elif status == 495:
-		return ("495 SSL Certificate Error (nginx)")
+		return ("SSL Certificate Error (nginx)")
 	elif status == 496:
-		return ("496 SSL Certificate Required (nginx)")
+		return ("SSL Certificate Required (nginx)")
 	elif status == 497:
-		return ("497 HTTP Request Sent to HTTPS Port (nginx)")
+		return ("HTTP Request Sent to HTTPS Port (nginx)")
 	elif status == 499:
-		return ("499 Client Closed Request (nginx)")
+		return ("Client Closed Request (nginx)")
 
 	#CloudFlare
 	elif status == 520:
-		return ("520 Unknown Error (CloudFlare)")
+		return ("Unknown Error (CloudFlare)")
 	elif status == 521:
-		return ("521 Web Server Is Down (CloudFlare)")
+		return ("Web Server Is Down (CloudFlare)")
 	elif status == 522:
-		return ("522 Connection Timed Out (CloudFlare)")
+		return ("Connection Timed Out (CloudFlare)")
 	elif status == 523:
-		return ("523 Origin Is Unreachable (CloudFlare)")
+		return ("Origin Is Unreachable (CloudFlare)")
 	elif status == 524:
-		return ("524 A Timeout Occurred (CloudFlare)")
+		return ("A Timeout Occurred (CloudFlare)")
 	elif status == 525:
-		return ("525 SSL Handshake Failed (CloudFlare)")
+		return ("SSL Handshake Failed (CloudFlare)")
 	elif status == 526:
-		return ("526 Invalid SSL Certificate (CloudFlare)")
+		return ("Invalid SSL Certificate (CloudFlare)")
--- website_checker.py
+++ website_checker.py
@@ -1,6 +1,7 @@
-#!/usr/local/bin/python
-import urllib
+import flask
+import urllib.request
 from status_codes import check_status
+import json
 
 #21 imports
 from two1.wallet import Wallet
@@ -9,6 +10,7 @@ import yaml
 
 app1 = flask.Flask(__name__)
 payment = Payment(app1, Wallet())
+app.debug = True
 
 #get website
 @app.route('/check_website/<string:website_url>')
@@ -17,17 +19,16 @@ def make_password(website_url = None):
 	website_url = website_url
 	url = "http://"+website_url
 
-	#get status code:
-	a = urllib.urlopen(url)
-	status = a.getcode()
-
 	try:
-		result = check_status(status)
-		message = " %s %s" %(website,result)
-		return (message)
+        #get status code:
+        a = urllib.request.urlopen(url)
+        status = a.getcode()
+		description = check_status(status)
+        message = {status : description}
+		return json.dumps(message)
 	except:
-		exception = " %s doesn't exist" % (website)
-		return (exception)
+        exception = {"Exception raised" : "Possibly %s doesn't exist" % (url)}
+		return json.dumps(exception)
 
 if __name__ == "__main__":
-    app1.run(host="0.0.0.0", port=3000)
+    app1.run(host="0.0.0.0", port=4000)
