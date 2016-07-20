d771c11e23a4fcbc584bacb88d26a7abce34e9af
dukakisxyz <kdukakis@icloud.com>
2016-07-20 18:32:55 +0300

    adding manifest route, minor fixes

--- website_checker.py
+++ website_checker.py
@@ -8,9 +8,14 @@ from two1.wallet import Wallet
 from two1.bitserv.flask import Payment
 import yaml
 
-app1 = flask.Flask(__name__)
-payment = Payment(app1, Wallet())
+app = flask.Flask(__name__)
+payment = Payment(app, Wallet())
 app.debug = True
+
+
+@app.route("/")
+def info():
+    return "To check the HTTP status of a website, run: 21 buy url http://10.244.183.245:4000/check_website/<www.website.com>
 
 #get website
 @app.route('/check_website/<string:website_url>')
@@ -30,5 +35,13 @@ def make_password(website_url = None):
         exception = {"Exception raised" : "Possibly %s doesn't exist" % (url)}
 		return json.dumps(exception)
 
+@app.route('/manifest')
+def manifest():
+    """Provide the app manifest to the 21 crawler.
+        """
+    with open('./manifest.yaml', 'r') as f:
+        manifest = yaml.load(f)
+    return json.dumps(manifest)
+
 if __name__ == "__main__":
-    app1.run(host="0.0.0.0", port=4000)
+    app.run(host="0.0.0.0", port=4000)
